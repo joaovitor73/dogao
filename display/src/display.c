3d69112928d13b286c86dbd73f06bc36198e9f81
joaovitor73 <joao.vieira.712@ufrn.edu.br>
2025-06-19 13:50:41 -0300

    feat: adiciona display

---
+++ display/src/display.c
@@ -0,0 +1,34 @@
+#include "../include/display.h"
+
+struct render_area frame_area = {
+    start_column : 0,
+    end_column : ssd1306_width - 1,
+    start_page : 0,
+    end_page : ssd1306_n_pages - 1
+};
+
+uint8_t ssd[ssd1306_buffer_length];
+
+void dpRestart(){
+    calculate_render_area_buffer_length(&frame_area);
+
+    memset(ssd, 0, ssd1306_buffer_length);
+    render_on_display(ssd, &frame_area);
+}
+
+void dpInit(){
+    i2c_init(i2c1, ssd1306_i2c_clock * 1000);
+    gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
+    gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
+    gpio_pull_up(I2C_SDA);
+    gpio_pull_up(I2C_SCL);
+
+    ssd1306_init();
+    dpRestart();
+}
+
+void dpWrite(char *mensage, int x, int y) {
+    dpRestart();
+    ssd1306_draw_string(ssd, x, y, mensage); 
+    render_on_display(ssd, &frame_area);   // Renderiza o buffer na tela
+}
--- main.c
+++ main.c
@@ -1,22 +1,26 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "neo/include/neopixel.h"
-
+#include "display/include/display.h"
 
 int main()
 {
     stdio_init_all();
     npInit();
-    npClear();
+    dpInit();
     uint cont = 0;
+    char text_buffer[5];
     while (true) {
         npSetLED(cont,0,0,240);
         npSetLED(cont+1,0,240,0);
         npSetLED(cont+2,240,0,0);
+        sprintf(text_buffer, "%u", cont); 
+        dpWrite(text_buffer, 60, 25);
         npWrite();
         sleep_ms(100);
         npClearRange(cont, cont+2);
         sleep_ms(100);
+        dpRestart();
         cont++;
         if(cont == 26) cont = 0;
     }
