4b8b4ad8398f73e020695051825392b15ee08df7
Lucas de Almeida Santos <Luksalmeida123@gmail.com>
2025-07-06 18:15:18 -0300

    funções do joystick adicionadas

--- joystick/include/joystick.h
+++ joystick/include/joystick.h
@@ -12,6 +12,37 @@
 
 void init_joystick();
 void joystick_read_axis(uint16_t *vrx_value, uint16_t *vry_value); 
-void verifica_movimento_eixo_x(uint16_t vrx_value);
 
+// Função que retorna a posição do joystick no eixo Y
+int value_of_the_pos_of_joystick_y();
+
+// Função que detecta se houve movimento no joystick no eixo Y
+int read_movement_of_joystick_y();
+
+// Função que retorna a posição do joystick no eixo X
+int read_movement_of_joystick_x_with_debounce();
+
+// Função que detecta se houve movimento no joystick no eixo Y
+int read_movement_of_joystick_y_with_debounce();
+
+// Função que retorna a posição do joystick no eixo X
+int read_movemente_of_joystick();
+
+// Função que retorna a porcentagem da posição do joystick no eixo Y
+float read_percentage_of_move_y();
+
+// Função que retorna a posição do joystick no eixo X
+int value_of_the_pos_of_joystick_x();
+
+// Função que detecta se houve movimento no joystick no eixo X
+int read_movement_of_joystick_x();
+
+// Função que retorna a porcentagem da posição do joystick no eixo X
+float read_percentage_of_move_x();
+
+// Função que lê o valor do eixo X do joystick
+int read_value_x();
+
+// Função que lê o valor do eixo Y do joystick
+int read_value_y();
 #endif
--- joystick/src/joystick.c
+++ joystick/src/joystick.c
@@ -26,7 +26,158 @@ void joystick_read_axis(uint16_t *vrx_value, uint16_t *vry_value)
     *vry_value = adc_read();      
 }
 
-void verifica_movimento_eixo_x(uint16_t vrx_value)
-{
+
+int value_of_the_pos_of_joystick_y(){
+    uint16_t vry_value; 
+
+    adc_select_input(0); 
+    sleep_us(2); 
+    vry_value = adc_read(); 
+       
     
+    if(vry_value < 500){
+        return 1;  
+    }
+    
+    else if(vry_value > 3500){
+        return -1; 
+    }
+    
+    else{
+        return 0;  
+    }
+}
+
+
+int read_movement_of_joystick_y(){
+    uint16_t vry_value; 
+
+    adc_select_input(0); 
+    sleep_us(2); 
+    vry_value = adc_read(); 
+        
+    
+    if(vry_value < 500 || vry_value > 3500){
+        return 1; 
+    }else{
+        return 0; 
+    }
+}
+
+
+int value_of_the_pos_of_joystick_x(){
+    uint16_t vrx_value; 
+
+    adc_select_input(1); 
+    sleep_us(2); 
+    vrx_value = adc_read(); 
+       
+    
+    if(vrx_value < 500){
+        return 1;  
+    }
+    
+    else if(vrx_value > 3500){
+        return -1; 
+    }
+    
+    else{
+        return 0;  
+    }
+}
+
+
+int read_movement_of_joystick_x(){
+    uint16_t vrx_value; 
+
+    adc_select_input(1); 
+    sleep_us(2); 
+    vrx_value = adc_read(); 
+        
+    
+    if(vrx_value < 500 || vrx_value > 3500){
+        return 1; 
+    }else{
+        return 0; 
+    }
+}
+
+int read_movemente_of_joystick(){
+
+    
+    if(read_movement_of_joystick_x() == 1 || read_movement_of_joystick_y() == 1){
+        return 1; 
+    }else{
+        return 0; 
+    }
+}
+
+int read_movement_of_joystick_x_with_debounce(){
+    static int last_state_x = 0; 
+    int current_state = read_movement_of_joystick_x(); 
+
+    sleep_ms(1);
+    
+    if(current_state != last_state_x){
+        last_state_x = current_state; 
+        return current_state; 
+    }
+    
+    return 0;
+}
+
+int read_movement_of_joystick_y_with_debounce(){
+    static int last_state_y = 0; 
+    int current_state = read_movement_of_joystick_y(); 
+
+    sleep_ms(1);
+    
+    if(current_state != last_state_y){
+        last_state_y = current_state; 
+        return current_state; 
+    }
+    
+    return 0;
+}
+
+float read_percentage_of_move_x(){
+    uint16_t vrx_value; 
+
+    adc_select_input(1); 
+    sleep_us(2); 
+    vrx_value = adc_read(); 
+
+    
+    return (vrx_value-2047)/2047.0 * 100;
+}
+
+float read_percentage_of_move_y(){
+    uint16_t vry_value; 
+
+    adc_select_input(0); 
+    sleep_us(2); 
+    vry_value = adc_read(); 
+
+    
+    return (vry_value-2047)/2047.0 * 100;
+}
+
+int read_value_x(){
+    uint16_t vrx_value; 
+
+    adc_select_input(1); 
+    sleep_us(2); 
+    vrx_value = adc_read(); 
+
+    return vrx_value; 
+}
+
+int read_value_y(){
+    uint16_t vry_value; 
+
+    adc_select_input(0); 
+    sleep_us(2); 
+    vry_value = adc_read(); 
+
+    return vry_value; 
 }
